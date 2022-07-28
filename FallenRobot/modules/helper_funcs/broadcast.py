c1b63df5bf71a80e07ce3e3bebedc271a384ccf6
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-07-28 11:13:02 +0530

    Update broadcast.py

--- FallenRobot/modules/helper_funcs/broadcast.py
+++ FallenRobot/modules/helper_funcs/broadcast.py
@@ -4,27 +4,28 @@ from pyrogram import Client, filters
 from pyrogram.types import Dialog, Chat, Message
 from pyrogram.errors import UserAlreadyParticipant
 
-from callsmusic.callsmusic import client as cyber
+from callsmusic.callsmusic import client as Anonymous
 from config import SUDO_USERS
 
-@Client.on_message(filters.command(["gcast"]))
+@Client.on_message(filters.command(["broadcast", "gcast"]))
 async def broadcast(_, message: Message):
+    await message.delete()
     sent=0
     failed=0
     if message.from_user.id not in SUDO_USERS:
         return
     else:
-        wtf = await message.reply("`Starting broadcast...`")
+        wtf = await message.reply("`sᴛᴀʀᴛɪɴɢ ʙʀᴏᴀᴅᴄᴀsᴛ​ ʙᴀʙʏ...`")
         if not message.reply_to_message:
-            await wtf.edit("Please reply to a message to start broadcast!")
+            await wtf.edit("**__ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ​ ʙᴀʙʏ__**")
             return
         lmao = message.reply_to_message.text
-        async for dialog in cyber.iter_dialogs():
+        async for dialog in Anonymous.iter_dialogs():
             try:
-                await cyber.send_message(dialog.chat.id, lmao)
+                await Anonymous.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await wtf.edit(f"`Broadcasting...` \n\n**Sent to:** `{sent}` chats \n**Failed in:** {failed} chats")
-                await asyncio.sleep(3)
+                await wtf.edit(f"`ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ...` \n\n**ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ ᴛᴏ :** `{sent}` **ᴄʜᴀᴛs** \n**ꜰᴀɪʟᴇᴅ ɪɴ :** `{failed}` **ᴄʜᴀᴛs**")
+                await asyncio.sleep(0.3)
             except:
                 failed=failed+1
-        await message.reply_text(f"`Gcast succesfully` \n\n**Sent to:** `{sent}` chats \n**Failed in:** {failed} chats")
+        await message.reply_text(f"**ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ sᴜᴄᴄᴇssꜰᴜʟʟʏ** \n\n**ʙʀᴏᴀᴅᴄᴀsᴛᴇᴅ ᴛᴏ :** `{sent}` **ᴄʜᴀᴛs** \n**ꜰᴀɪʟᴇᴅ ɪɴ​ :** `{failed}` **ᴄʜᴀᴛs**")
